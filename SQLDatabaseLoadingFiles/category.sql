5729c747d35a2bf9ee9df476b020610aa203d6fa
petrero <petrero@gmail.com>
2021-08-19 10:54:34 +0300

    SQL files that help to load database with data. They must be run in this order: author.sql, publisher.sql, category.sql, sub_category.sql, book.sql, book_author.sql, book_publisher.sql

---
+++ SQLDatabaseLoadingFiles/category.sql
@@ -0,0 +1,23 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: books
+-- ------------------------------------------------------
+-- Server version	5.7.31-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `category`
+--
+
+
+INSERT INTO `books_vue`.`category` VALUES (1,'literatura'),(2,'arta'),(3,'stiinta'),(4,'tehnica');
---
+++ SQLDatabaseLoadingFiles/sub_category.sql
@@ -0,0 +1,22 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: books
+-- ------------------------------------------------------
+-- Server version	5.7.31-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `sub_category`
+--
+
+INSERT INTO `books_vue`.`sub_category` VALUES (1,1,'roman'),(2,1,'poem'),(3,1,'stiintifico fantastica'),(4,1,'jurnal de calatorie'),(5,NULL,'ozn, extraterestrii'),(6,NULL,'pamflet, satira'),(7,NULL,'economie, politica, social'),(8,NULL,'paranormal'),(9,1,'roman politist'),(10,NULL,'istorie'),(12,1,'poezie'),(13,2,'pictura'),(14,1,'anticipatie'),(15,1,'teatru'),(16,NULL,'dictionar'),(17,4,'calculatoare'),(18,NULL,'ghid practic'),(19,3,'matematica'),(20,3,'explorare, geografie'),(21,NULL,'lingvistica'),(23,4,'aviatie'),(24,3,'popularizare a stiintei'),(25,NULL,'mituri si legende'),(26,3,'paleontologie'),(27,2,'arhitectura'),(28,NULL,'turism'),(29,3,'dezastre'),(30,2,'fotografie'),(31,NULL,'patrimoniu'),(32,3,'anatomie'),(33,1,'folclor'),(35,3,'medicina'),(36,3,'biologie'),(37,NULL,'limbi straine'),(38,4,'electronica'),(39,4,'tehnica'),(40,1,'copii'),(43,1,'critica'),(44,NULL,'pescuit, vanatoare'),(45,1,'interviu'),(46,1,'actiune, aventuri'),(47,1,'poezie, proza'),(48,NULL,'pedagogie'),(50,NULL,'religie'),(51,1,'ghicitori, proverbe, zicatori'),(53,NULL,'sport'),(55,NULL,'monumente, muzee'),(56,3,'fizica'),(57,1,'povestiri'),(58,NULL,'muzica'),(59,NULL,'circulatie'),(60,3,'astronautica'),(62,2,'pictura, sculptura, arhitectura'),(63,1,'reportaj'),(64,NULL,'agricultura'),(65,NULL,'bucatarie'),(66,3,'geologie'),(67,3,'umanista'),(68,3,'astronomie, cosmologie'),(69,1,'publicistica'),(70,1,'antologie'),(71,1,'scrisori'),(72,1,'biografie'),(73,1,'literatura'),(75,1,'povesti'),(76,1,'umor'),(77,1,'nuvele'),(78,2,'film'),(79,1,'critica si text'),(80,1,'stilistica'),(81,3,'stiinta'),(82,1,'proza'),(83,3,'psihologie'),(84,NULL,'filozofie'),(85,3,'antropologie'),(86,1,'evocari'),(87,1,'fantastica'),(88,NULL,'juridic'),(89,NULL,'manual'),(90,1,'crestomatie'),(91,NULL,'ezoterica'),(92,NULL,'eseuri'),(93,1,'anecdote'),(94,NULL,'almanah'),(95,NULL,'enciclopedie'),(96,3,'nutritie'),(97,1,'epopee'),(98,NULL,'revista'),(99,NULL,'banda desenata'),(100,1,'memorii'),(101,1,'aforisme');
